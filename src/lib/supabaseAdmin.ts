9b9aaff5e164de6ddd03ec810b9ab79c184e5ffb
lashen333 <lashen.aenigm3labs@gmail.com>
2026-02-20 01:04:35 +0530

    supabase issue fix

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -2,7 +2,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const Url = process.env.SUPABASE_URL!;
-const serviceKey = process.env.SUPABASE_SERVICE_KEY!;
+console.log("Supabase URL:", Url);
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+console.log("Supabase Service Role Key:", serviceKey ? "Loaded" : "Missing");
 
 //Service role key must ONLY be used server-side
 export const supabaseAdmin = createClient(Url, serviceKey,{
